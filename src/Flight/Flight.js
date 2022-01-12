4592aa9f2bdcfaf032b626ab655bf8eb488d44bd
RomanSaykevich <saykevichroman628@gmail.com>
2022-01-12 02:43:17 +0200

    zr

---
+++ src/Flight/Flight.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Flight = (props) => {
+    const {mission_name, launch_year, mission_patch_small}= props;
+    return (
+        <div className={'fig'}>
+            <div className={'fit'}>
+            <h3>{mission_name}</h3>  {launch_year}
+            </div>
+            <img src={mission_patch_small} alt={mission_patch_small}/>
+        </div>
+    );
+};
+
+export default Flight;
---
+++ src/Flights/Flights.js
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react';
+import Flight from "../Flight/Flight";
+const Flights = () => {
+    const [flights, setflights] = useState([]);
+    useEffect(()=>{
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(value => value.json())
+            .then(value => {
+                setflights(value.filter(flight => flight.launch_year !== '2020'))
+            });
+    },[])
+    return (
+        <div>
+            {
+                flights.map(value => <Flight mission_name={value.mission_name}
+                                          launch_year={value.launch_year}
+                                          mission_patch_small={value.links.mission_patch_small}
+
+                />
+                )
+                }
+        </div>
+    );
+};
+
+export default Flights;
